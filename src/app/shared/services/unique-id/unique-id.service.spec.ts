b9e6326445017b335e4b08fd202c40384613a09d
vicduraes <duraes.vic@gmail.com>
2021-05-07 18:52:53 -0300

    chore: create service to generate components ids

---
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -0,0 +1,32 @@
+import { UniqueIdService } from './unique-id.service';
+describe(UniqueIdService.name, () => {
+  let service: UniqueIdService;
+
+  beforeEach(() => {
+    service = new UniqueIdService();
+  });
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should generate id when called with prefix`, () => {
+    const id = service.generateUniqueIdWithPrefix('app');
+    expect(id.startsWith('app-')).toBeTrue();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should not generate duplicate IDs when called multiple times`, () => {
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toEqual(50);
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+  should throw when called with empty prefix`, () => {
+    const emptyValues = [null, undefined, '', '0', '1'];
+    emptyValues.forEach((emptyValue) => {
+      expect(() => service.generateUniqueIdWithPrefix(emptyValue))
+        .withContext(`Empty value: ${emptyValue}`)
+        .toThrow();
+    });
+  });
+});
